253860e9337376b23fdec1664dfc7f6f5e0b97be
John Canero <johncanero.official@gmail.com>
2022-08-09 12:44:43 +0800

    Include Update Files of Changing Complex State

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,9 +1,43 @@
+import { useState } from "react";
+
 const Hello = () => {
+
+    const [fName, setFName] = useState("");
+    const [lName, setLName] = useState("");
+    
+    const updateFName = (event) => {
+        const firstName = event.target.value;
+        setFName(firstName);
+    };
+    
+    const updateLName = (event) => {
+        const lastName = event.target.value;
+        setLName(lastName);
+    };
+
   return (
-    <div>
-         <h1>Hello</h1> 
-    </div>
-  )
+    <div className="container">
+        <h1>
+            Hello {fName} {lName}
+        </h1>
+        <form>
+            <input
+                name="fName"
+                onChange={updateFName}
+                placeholder="First Name"
+                value={fName}
+            />
+
+            <input
+                name="lName"
+                onChange={updateLName}
+                placeholder="Last Name"
+                value={lName}
+            />
+            <button>Submit</button>
+        </form>
+  </div>
+  );
 }
 
 export default Hello

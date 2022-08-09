162798a752250b2f4725861bf364ed4f644b57c1
John Canero <johncanero.official@gmail.com>
2022-08-09 18:34:39 +0800

    Create and Modify Comments for Better Understanding

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,40 +2,52 @@ import { useState } from "react";
 
 const Hello = () => {
 
-    // State Hook
-    const [fName, setFName] = useState("");
-    const [lName, setLName] = useState("");
-    
-    // Update First Name and Last Name
-    const updateFName = (event) => {
-        const firstName = event.target.value;
-        setFName(firstName);
-    };
-    
-    const updateLName = (event) => {
-        const lastName = event.target.value;
-        setLName(lastName);
-    };
+    // Create a const with fullName and setFullName, Use State Hook with fName and lName
+    const [fullName, setFullName] = useState({
+        fName: "",
+        lName: ""
+    });
+
+    // Create a const handleChange and add an event
+    // Create a const with value and name and use it in (if-else if statement)
+    const handleChange = (event) => {
+        const {value, name} = event.target
+
+        setFullName((prevValue) => {
+            if (name === "fName") {
+                return {
+                    fName: value,
+                    lName: prevValue.lName
+                }
+            } else if (name === "lName") {
+                return {
+                    fName: prevValue.fName, 
+                    lName: value
+                }
+            }
+        });
+    }  
 
   return (
     <div className="container">
+        {/* Input the const.[fName] and const[lName] in the Heading */}
         <h1>
-            Hello {fName} {lName}
+            Hello {fullName.fName} {fullName.lName}
         </h1>
         <form>
-            {/* onChange and value */}
+            {/* Input onChange and value to make the function work */}
             <input
                 name="fName"
-                onChange={updateFName}
+                onChange={handleChange}
                 placeholder="First Name"
-                value={fName}
+                value={fullName.fName}
             />
 
             <input
                 name="lName"
-                onChange={updateLName}
+                onChange={handleChange}
                 placeholder="Last Name"
-                value={lName}
+                value={fullName.lName}
             />
             <button>Submit</button>
         </form>
